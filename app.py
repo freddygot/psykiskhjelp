5ff4227fdf5f3cb91e376aabea172c2afadf098f
Fredrik Lian <fredriklian@Fredrik-sin-MacBook-Air.local>
2023-11-13 08:34:04 +0100

    Lagt til lat long, postalcode og city i databasen

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Importer Migrate-klassen
 from models import db
 from routes import routes
 from config import Config
@@ -8,6 +10,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    migrate = Migrate(app, db)
+
 
     with app.app_context():
         db.create_all()  # Oppretter databasetabeller
--- models.py
+++ models.py
@@ -7,6 +7,10 @@ class Psychologist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(300))
+    postal_code = db.Column(db.String(10))  # Postnummer
+    city = db.Column(db.String(64))  # By
+    latitude = db.Column(db.Float)  # Latitude for geolokasjon
+    longitude = db.Column(db.Float)  # Longitude for geolokasjon
     country = db.Column(db.String(50))
     language = db.Column(db.String(50))
     email = db.Column(db.String(120), unique=True)
